714fa9192b5f4b982e95e4ee73741ea16ef9b445
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-08-15 22:33:40 +0530

    Update

--- install_kernel.sh
+++ install_kernel.sh
@@ -8,20 +8,20 @@ echo "Enter the name of the kernel: "
 read kernel
 
 #COPING VMLINUX
-cp -v arch/x86_64/boot/bzImage /boot/vmlinux-$kernel
+cp -v arch/x86_64/boot/bzImage /boot/vmlinuz-$kernel
 
 #CREATING PRESET
 touch /etc/mkinitcpio.d/$kernel.preset
-echo "#mkinitcpio preset file for the '"'linux'"' package" >> /etc/mkinitcpio.d/$kernel.preset
+echo "# mkinitcpio preset file for the '"'linux'"' package" >> /etc/mkinitcpio.d/$kernel.preset
 echo ALL_config='"'/etc/mkinitcpio.conf'"' >> /etc/mkinitcpio.d/$kernel.preset
-echo ALL_KVER='"'/boot/vmlinux-$kernel'"' >> /etc/mkinitcpio.d/$kernel.preset
-echo "PRESETS=('"'defaults'"' '"'fallback'"')" >> /etc/mkinitcpio.d/$kernel.preset
+echo ALL_kver='"'/boot/vmlinuz-$kernel'"' >> /etc/mkinitcpio.d/$kernel.preset
+echo "PRESETS=('"'default'"' '"'fallback'"')" >> /etc/mkinitcpio.d/$kernel.preset
 echo '#'default_config='"'/etc/mkinitcpio.conf'"' >> /etc/mkinitcpio.d/$kernel.preset
 echo default_image='"'/boot/initramfs-$kernel.img'"' >> /etc/mkinitcpio.d/$kernel.preset
 echo '#'default_options='"''"' >> /etc/mkinitcpio.d/$kernel.preset
 echo '#'fallback_config='"'/etc/mkinitcpio.conf'"' >> /etc/mkinitcpio.d/$kernel.preset
 echo fallback_image='"'/boot/initramfs-$kernel-fallback.img'"' >> /etc/mkinitcpio.d/$kernel.preset
-echo fallback_option='"'-S autodetect'"' >> /etc/mkinitcpio.d/$kernel.preset
+echo fallback_options='"'-S autodetect'"' >> /etc/mkinitcpio.d/$kernel.preset
 
 #GENERATING INITIAL RAM DISK
 mkinitcpio -p $kernel
